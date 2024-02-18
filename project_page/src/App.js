50a523fc815dcf73277b84bf69c9df5313672214
Sughanthan <A022417927@hepl.local>
2024-02-18 14:56:17 +0530

    Initial commit

---
+++ project_page/src/App.js
@@ -0,0 +1,50 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="total">
+
+      <div className="topleft">
+      </div>
+      <div className="topright">
+      </div>
+      <div className="Main"> 
+      <div className="Title">
+          <div className="ToDoTitle A">
+            <div className="ToDo03 B" >
+              <div>To Do <span>(03)</span></div>
+              <div className="Blk C"></div>
+            </div>
+            <div className="Plus1 D">+ ...</div>  
+          </div>
+          <div className="ProgressTitle A">
+            <div className="Progress02 B"> 
+              <div>In Progress <span>(02)</span></div>
+              <div className="Ble C"></div>                     
+            </div>
+            <div className="Plus2 D">+ ...</div>  
+          </div>
+          <div className="ReviewTitle A">
+            <div className="Review10 B">
+              <div >In Review <span>(10)</span></div>
+             <div className="Yel C"></div>
+            </div>
+            <div className="Plus3 D">+ ...</div>  
+      </div>
+        
+      </div>
+        
+      
+      
+      
+      
+      
+      
+      </div>
+     
+    </div>
+  );
+}
+
+export default App;
